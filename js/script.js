706fc5fe28014697ab22b7965c6aea3842728c33
Mr-Macko <max.allesch@gmx.de>
2021-11-13 22:06:02 +0100

    added to-do-list functionality

---
+++ js/script.js
@@ -0,0 +1,30 @@
+//1. Adding a new item to the list of items
+// creates new list item
+let li = $('<li></li>');
+let inputValue = $('#input').val();
+li.append(inputValue);
+
+if (inputValue === '') {
+  alert("You must write something!");
+} else {
+  $('#list').append(li);
+}
+
+ //2. Crossing out an item from the list of items:
+ function crossOut() {
+    li.toggleClass("strike");
+}
+
+li.on("dblclick", function crossOut() {
+    li.toggleClass("strike");
+});
+//3(i). Adding the delete button "X": 
+let crossOutButton = $('<crossOutButton></crossOutButton>');
+crossOutButton.append(document.createTextNode('X'));
+li.append(crossOutButton);
+
+   crossOutButton.on("click", deleteListItem);
+   function deleteListItem(){
+ 		li.addClass("delete")
+}
+ $('#list').sortable();
